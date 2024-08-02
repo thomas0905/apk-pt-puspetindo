56d1905723acb54fc08a3e7a00953f9f3a5a8436
Muhammad.Rois <111403383+roismohammed@users.noreply.github.com>
2024-08-02 13:29:30 +0700

    sdf

---
+++ inertia/pages/admin/users/menuProfil.tsx
@@ -0,0 +1,11 @@
+import { Head } from '@inertiajs/react'
+import React from 'react'
+import Admin from '~/layout/admin'
+
+export default function MenuProfil() {
+  return (
+    <Admin>
+      <Head title='menu-profil'/>
+    </Admin>
+  )
+}
--- start/routes.ts
+++ start/routes.ts
@@ -1,6 +1,5 @@
 import router from '@adonisjs/core/services/router'
 const DasboardsController =() => import('#controllers/dasboards_controller')
-const SistemsController =() => import('#controllers/sistem_controller')
 
 router.on('/').renderInertia('home', { version: 6 })
 
@@ -8,12 +7,6 @@ router.get('dasboard/pengguna/pengguna',[DasboardsController,'pengguna'])
 router.get('dasboard/pengguna/create',[DasboardsController,'create'])
 router.post('dasboard/pengguna/create',[DasboardsController,'store'])
 router.get('dasboard/proyek',[DasboardsController,'proyek'])
-
-
-router.get('sistem/pengguna/edit',[SistemsController,'edit'])
-router.get('sistem/pengaturan',[SistemsController,'pengaturan'])
-
-
 
 
 
