e21fb8ad3868032b97efe4615ed958b8ad1633e6
Muhammad.Rois <111403383+roismohammed@users.noreply.github.com>
2024-08-02 13:48:00 +0700

    sdfsdf

--- inertia/pages/admin/dasboard/proyek.tsx
+++ inertia/pages/admin/dasboard/proyek.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 import Admin from '~/layout/admin'
 
 export default function Proyek() {
-  return (
-    <Admin>
-      <Head title='proyek'/>
-      Halaman Proyek
-    </Admin>
-  )
+    return (
+        <Admin>
+            <Head title='proyek' />
+            Halaman Proyek
+        </Admin>
+    )
 }
