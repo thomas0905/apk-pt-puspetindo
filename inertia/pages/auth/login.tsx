353d5c9b960019a0a937b96aca7a3714b1fb103c
Muhammad.Rois <111403383+roismohammed@users.noreply.github.com>
2024-08-05 09:51:21 +0700

    sdf

--- inertia/pages/auth/login.tsx
+++ inertia/pages/auth/login.tsx
@@ -1,34 +1,40 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
 export default function Login() {
     return (
-        <div>
-            <Card className="w-full max-w-sm">
+        <div className='flex justify-center items-center h-screen'>
+            <Card className="w-full max-w-md border-0 shadow-md">
                 <CardHeader>
-                    <CardTitle className="text-2xl">Login</CardTitle>
+                    <CardTitle className="text-3xl">Login</CardTitle>
                     <CardDescription>
-                        Enter your email below to login to your account.
+                    Hai 👏, Selamat Datang Kembali di PT.Pustpetindo
                     </CardDescription>
                 </CardHeader>
                 <CardContent className="grid gap-4">
                     <div className="grid gap-2">
-                        <Label htmlFor="email">Email</Label>
-                        <Input id="email" type="email" placeholder="m@example.com" required />
+                        <Label htmlFor="email">Username</Label>
+                        <Input id="email" type="email" placeholder="Email" required />
                     </div>
+
+                    <div className="grid gap-2">
+                        <Label htmlFor="email">Email</Label>
+                        <Input id="email" type="email" placeholder="Email" required />
+                    </div>
+                    
                     <div className="grid gap-2">
                         <Label htmlFor="password">Password</Label>
-                        <Input id="password" type="password" required />
+                        <Input id="password" type="password" required  placeholder='Password'/>
                     </div>
                 </CardContent>
                 <CardFooter>
