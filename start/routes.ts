940a5a53ca3fa4fa988ac4cb13c8a60a29ed19b5
Muhammad.Rois <111403383+roismohammed@users.noreply.github.com>
2024-08-06 08:59:56 +0700

    sdf

--- start/routes.ts
+++ start/routes.ts
@@ -1,23 +1,26 @@
 import ManagementsController from '#controllers/managements_controller'
 import router from '@adonisjs/core/services/router'
-const DasboardsController =() => import('#controllers/dasboards_controller')
-const UsersController =() => import('#controllers/users_controller')
+const DasboardsController = () => import('#controllers/dasboards_controller')
+const UsersController = () => import('#controllers/users_controller')
 router.on('/').renderInertia('home', { version: 6 })
 
-router.get('/dasboard/pengguna/pengguna',[DasboardsController,'pengguna'])
-router.get('/dasboard/pengguna/create',[DasboardsController,'create'])
-router.post('/dasboard/pengguna/create',[DasboardsController,'store'])
-router.delete('dasboard/pengguna/pengguna/:id',[DasboardsController,'delete'])
-router.get('/dasboard/pengguna/edit/:id', [DasboardsController,'edit'])
-router.put('/dasboard/pengguna/edit/:id', [DasboardsController, 'update'])
 
-router.get('/dasboard/proyek/index',[DasboardsController,'index'])
+router.group(() => {
+    router.get('pengguna', [DasboardsController, 'pengguna'])
+    router.get('create', [DasboardsController, 'create'])
+    router.post('create', [DasboardsController, 'store'])
+    router.delete('pengguna/:id', [DasboardsController, 'delete'])
+    router.get('edit/:id', [DasboardsController, 'edit'])
+    router.put('edit/:id', [DasboardsController, 'update'])
+}).prefix('dasboard/pengguna/')
 
-router.get('/auth/login',[DasboardsController,'login'])
+router.get('/dasboard/proyek/index', [DasboardsController, 'index'])
 
-router.get('/users/menuprofil',[UsersController,'menuprofil'])
-router.get('/users/minhours',[UsersController,'minhours'])
+router.get('/auth/login', [DasboardsController, 'login'])
+
+router.get('/users/menuprofil', [UsersController, 'menuprofil'])
+router.get('/users/minhours', [UsersController, 'minhours'])
 
 
-router.get('/management/laporan',[ManagementsController,'management'])
+router.get('/management/laporan', [ManagementsController, 'management'])
 
