ebb17073f80906100307a55cbc849b76f7ffaff9
Muhammad.Rois <111403383+roismohammed@users.noreply.github.com>
2024-08-10 23:04:58 +0700

    fasdf

--- inertia/pages/admin/sistem/pengguna/permission.tsx
+++ inertia/pages/admin/sistem/pengguna/permission.tsx
@@ -5,6 +5,18 @@ import { Button } from '~/components/ui/button'
 import { Card } from '~/components/ui/card'
 import Admin from '~/layout/admin'
 
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { Label } from '~/components/ui/label'
+import { Input } from '~/components/ui/input'
+
 export default function Permission() {
   return (
     <Admin>
@@ -18,21 +30,68 @@ export default function Permission() {
               </Link>
               <h6 className='text-gray-600 text-lg font-bold'>Atur Permission</h6>
             </div>
-
-
-            <div >
-              <Link href='#' className='flex gap-2'>
-                <Button className=" hover:bg-gray-50 border text-black btn-small gap-2 " variant="outline">
-                  Cancel
-                </Button>
-                <Button className="bg-blue-600 hover:bg-blue-500 text-white btn-small gap-2 hover:text-white" variant="outline">
-                  Save Changes
-                </Button>
-
-              </Link>
-            </div>
           </div>
         </div>
+
+        <Card className='mt-3 rounded-none'>
+          <div className="border-b border-gray-200 p-2">
+            <div className='flex justify-between'>
+              <div>
+                <h6 className='text-gray-600 text-lg font-bold'>Karyawan</h6>
+              </div>
+
+
+              <div >
+                <Link href='#' className='flex gap-2'>
+                  <Button className=" hover:bg-gray-50 border text-black btn-small gap-2 " variant="outline">
+                    Cancel
+                  </Button>
+                  <Button className="bg-blue-600 hover:bg-blue-500 text-white btn-small gap-2 hover:text-white" variant="outline">
+                    Save Changes
+                  </Button>
+
+                </Link>
+              </div>
+            </div>
+          </div>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Username</TableHead>
+                <TableHead>Create</TableHead>
+                <TableHead>Update</TableHead>
+                <TableHead>Delete</TableHead>
+                <TableHead>Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="font-medium">Muhammad Rois</TableCell>
+                <TableCell className="font-medium text-center">
+                  <Label className="flex mx-2 justify-center items-center bg-gray-100 w-6 h-6">
+                    <Input type="checkbox" id=""></Input>
+                  </Label>
+                </TableCell>
+                <TableCell className="text-center">
+                  <Label className="flex mx-2 justify-center items-center bg-gray-100 w-6 h-6">
+                    <Input type="checkbox" id=""></Input>
+                  </Label>
+                </TableCell>
+                <TableCell className="text-center">
+                  <Label className="flex mx-2 justify-center items-center bg-gray-100 w-6 h-6">
+                    <Input type="checkbox" id=""></Input>
+                  </Label>
+                </TableCell>
+                <TableCell className="text-center">
+                  <Label className="flex  mx-2 justify-center items-center bg-gray-100 w-6 h-6">
+                    <Input type="checkbox" id=""></Input>
+                  </Label>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+
+        </Card>
       </Card>
     </Admin>
   )
