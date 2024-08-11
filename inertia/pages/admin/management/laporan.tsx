f30be5dd25fc7741f16bd76eb2fa38c363db6e6e
Muhammad.Rois <111403383+roismohammed@users.noreply.github.com>
2024-08-11 22:15:45 +0700

    asdfasf

--- inertia/pages/admin/management/laporan.tsx
+++ inertia/pages/admin/management/laporan.tsx
@@ -1,9 +1,10 @@
 import { Head, Link } from '@inertiajs/react'
-import { IconHome, IconUserPlus } from '@tabler/icons-react'
+import { IconHome } from '@tabler/icons-react'
 import React from 'react'
 import { Button } from '~/components/ui/button'
 import { Card } from '~/components/ui/card'
 import Admin from '~/layout/admin'
+import logoPuspetindo from '../../../img/logo-puspetindo.png'
 
 export default function Laporan() {
   return (
@@ -11,6 +12,9 @@ export default function Laporan() {
       <Head title='laporan' />
       <Card className="p-5">
         <div className="border-b border-gray-200 pb-4">
+          <div className='flex justify-center'>
+            <img src={logoPuspetindo} alt="" />
+          </div>
           <div className='flex justify-between'>
             <div>
               <Link href="/">
