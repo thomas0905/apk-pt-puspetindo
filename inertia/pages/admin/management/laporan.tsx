199b024b9eb50ec5df171bbee180395faf218426
Muhammad.Rois <111403383+roismohammed@users.noreply.github.com>
2024-08-11 21:01:50 +0700

    sd

--- inertia/pages/admin/management/laporan.tsx
+++ inertia/pages/admin/management/laporan.tsx
@@ -1,12 +1,26 @@
-import { Head } from '@inertiajs/react'
+import { Head, Link } from '@inertiajs/react'
+import { IconHome, IconUserPlus } from '@tabler/icons-react'
 import React from 'react'
+import { Button } from '~/components/ui/button'
+import { Card } from '~/components/ui/card'
 import Admin from '~/layout/admin'
 
 export default function Laporan() {
   return (
     <Admin>
-      <Head title='laporan'/>
-      Halaman Laporan
+      <Head title='laporan' />
+      <Card className="p-5">
+        <div className="border-b border-gray-200 pb-4">
+          <div className='flex justify-between'>
+            <div>
+              <Link href="/">
+                <p className='text-sm flex gap-1'><IconHome size={18} />Home</p>
+              </Link>
+              <h6 className='text-gray-600 text-lg font-bold'>Laporan</h6>
+            </div>
+          </div>
+        </div>
+      </Card>
     </Admin>
   )
 }
