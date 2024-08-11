74756860b089f7f16301faf43709d16632206efd
Muhammad.Rois <111403383+roismohammed@users.noreply.github.com>
2024-08-11 13:33:47 +0700

    Update 1722915210749_create_karyawans_table.ts

--- database/migrations/1722915210749_create_karyawans_table.ts
+++ database/migrations/1722915210749_create_karyawans_table.ts
@@ -7,7 +7,7 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.integer('user_id').unsigned().references('users.id').onDelete('CASCADE')
-      table.string('nama')
+      table.string('nama').unique()
       table.string('departemen')
       table.string('jabatan')
       table.string('status')
