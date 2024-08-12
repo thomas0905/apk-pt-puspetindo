9cbe4c520ef26ed0cddfa62296acb92f7be18218
Muhammad.Rois <111403383+roismohammed@users.noreply.github.com>
2024-08-12 08:24:32 +0700

    sdf

--- inertia/pages/admin/management/laporan.tsx
+++ inertia/pages/admin/management/laporan.tsx
@@ -1,37 +1,52 @@
-import { Head, Link } from '@inertiajs/react'
-import { IconHome, IconPrinter } from '@tabler/icons-react'
-import React from 'react'
+import { Head } from '@inertiajs/react'
+import { IconPrinter } from '@tabler/icons-react'
+import React, { useRef } from 'react'
 import { Button } from '~/components/ui/button'
 import { Card } from '~/components/ui/card'
 import Admin from '~/layout/admin'
 import logoPuspetindo from '../../../img/logo-puspetindo.png'
+import ReactToPrint from 'react-to-print';
+import Swal from 'sweetalert2'
 
 export default function Laporan() {
+  const componentRef = useRef(null);
+
+  const handlePrint = () => {
+    Swal.fire({
+      title: 'Data Berhasil Di Tambah!',
+      icon: 'success',
+      confirmButtonText: 'Okee',
+  });
+  }
   return (
     <Admin>
-      <Head title='laporan' />
-      <Card className="p-5">
-        <div className="border-b border-gray-200 pb-4">
-          <div className='flex justify-center'>
-            <img src={logoPuspetindo} alt="" />
-          </div>
-          <div className='flex justify-between'>
-            <div>
-              {/* <Link href="/">
-                <p className='text-sm flex gap-1'><IconHome size={18} />Home</p>
-              </Link> */}
-              <h6 className='text-gray-600 text-lg font-bold'>Laporan</h6>
+
+      <Head title='Laporan' />
+      {/* Pastikan Card atau elemen DOM lainnya yang menerima ref */}
+      <div ref={componentRef}>
+        <Card className="p-5">
+          <div className="border-b border-gray-200 pb-4">
+            <div className='flex justify-center'>
+              <img src={logoPuspetindo} alt="Logo Puspetindo" />
+            </div>
+            <div className='flex justify-between'>
+              <div>
+                <h6 className='text-gray-600 text-lg font-bold'>Laporan</h6>
+              </div>
             </div>
           </div>
-
-        </div>
-        <div className='flex justify-end mt-3'>
-          <Button className='bg-blue-500 flex gap-1 hover:bg-blue-400'>
-            <IconPrinter />
-            Print
-          </Button>
-        </div>
-      </Card>
+          <div className='mt-2 flex justify-end'>
+            <ReactToPrint
+              trigger={() => {
+                return <Button onClick={handlePrint} className='bg-blue-500 flex gap-1 hover:bg-blue-400'><IconPrinter /> Print</Button>
+              }}
+              content={() => componentRef.current}
+              documentTitle='Laporan Puspetindo'
+              pageStyle="print"
+            />
+          </div>
+        </Card>
+      </div>
     </Admin>
   )
 }
