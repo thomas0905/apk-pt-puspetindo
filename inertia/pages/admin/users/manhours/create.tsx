413cb68c978604213aa56fb48a96ad2c85b91973
Muhammad.Rois <111403383+roismohammed@users.noreply.github.com>
2024-08-15 15:42:17 +0700

    Update create.tsx

--- inertia/pages/admin/users/manhours/create.tsx
+++ inertia/pages/admin/users/manhours/create.tsx
@@ -61,41 +61,41 @@ export default function Create() {
             <div className="flex flex-col space-y-1.5">
               <Label>Pilih Karyawan:</Label>
               <Select
-                value={data.karyawan_id}
                 onValueChange={(value) => setData('karyawan_id', value)}
               >
                 <SelectTrigger className="w-full">
-                  <SelectValue placeholder="Pilih Karyawan" />
+                  <SelectValue placeholder="Pilih karyawan" />
                 </SelectTrigger>
                 <SelectContent>
                   {data_karyawan.map((kar) => (
-                    <SelectItem key={kar.id} value={kar.id}>
+                    <SelectItem key={kar.id} value={kar.id.toString()}> {/* Ubah value menjadi id departemen */}
                       {kar.nama}
                     </SelectItem>
                   ))}
                 </SelectContent>
               </Select>
+              {/* {errors.departemen_Id && <small className="text-red-600">{errors.departemen_Id}</small>} */}
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-3">
-              <div className="flex flex-col space-y-1.5">
-                <Label>Pilih Proyek:</Label>
-                <Select
-                  value={data.proyek_id}
-                  onValueChange={(value) => setData('proyek_id', value)}
-                >
-                  <SelectTrigger className="w-full">
-                    <SelectValue placeholder="Pilih Proyek" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {data_proyek.map((proyek) => (
-                      <SelectItem key={proyek.id} value={proyek.id}>
-                        {proyek.namaProyek}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-              </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label>Pilih Proyek:</Label>
+              <Select
+                onValueChange={(value) => setData('proyek_id', value)}
+              >
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Pilih Proyek" />
+                </SelectTrigger>
+                <SelectContent>
+                  {data_proyek.map((pro) => (
+                    <SelectItem key={pro.id} value={pro.id.toString()}> {/* Ubah value menjadi id departemen */}
+                      {pro.namaProyek}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              {/* {errors.departemen_Id && <small className="text-red-600">{errors.departemen_Id}</small>} */}
+            </div>
 
               <div className="flex flex-col space-y-1.5">
                 <Label>Tanggal:</Label>
