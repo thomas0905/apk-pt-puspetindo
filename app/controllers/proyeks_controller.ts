ed4ead7ced1f60e259cac551046657f3984ec52c
Muhammad.Rois <111403383+roismohammed@users.noreply.github.com>
2024-08-16 11:04:33 +0700

    Update proyeks_controller.ts

--- app/controllers/proyeks_controller.ts
+++ app/controllers/proyeks_controller.ts
@@ -1,17 +1,35 @@
+import Karyawan from '#models/karyawan';
 import Proyek from '#models/proyek'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class ProyeksController {
-    async index({inertia}:HttpContext){
-        const proyek = await Proyek.all() 
-        console.log(proyek);
-        
-        return inertia.render('admin/dasboard/proyek/index',{
-            data_proyek:proyek
-        })
+    async index({ inertia, auth }: HttpContext) {
+        // Mendapatkan user yang terotentikasi
+        const user = auth.user;
+
+        // Periksa apakah user terotentikasi
+        if (!user) {
+            return inertia.render('admin/error/404');
+        }
+
+        // Mengambil data karyawan berdasarkan user_id
+        const karyawan = await Karyawan.query().where('user_id', user.id).first();
+
+        // Periksa apakah karyawan ada dan memiliki jabatan yang sesuai
+        if (!karyawan || karyawan.jabatan !== 'IT Software') {
+            return inertia.render('admin/error/404');
+        }
+
+        // Mengambil data proyek
+        const proyek = await Proyek.all();
+
+        // Menampilkan data proyek pada halaman yang sesuai
+        return inertia.render('admin/dasboard/proyek/index', {
+            data_proyek: proyek
+        });
     }
 
-    async create({inertia}:HttpContext){
+    async create({ inertia }: HttpContext) {
         return inertia.render('admin/dasboard/proyek/create')
     }
 
@@ -33,7 +51,7 @@ export default class ProyeksController {
         const proyek = await Proyek.findOrFail(params.id)
         await proyek.delete()
         return response.redirect('/proyek')
-    } 
+    }
 
     async edit({ inertia, params }: HttpContext) {
         console.log(params.id);
