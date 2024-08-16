44520bb93dbc9113ec13351d5d43dd310c47e131
Muhammad.Rois <111403383+roismohammed@users.noreply.github.com>
2024-08-16 09:43:39 +0700

    sdf

--- inertia/pages/admin/error/404.tsx
+++ inertia/pages/admin/error/404.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Admin from '~/layout/admin'
 import './404.css'
+import img404 from '../../../img/404.png'
+
 export default function No_akses() {
     return (
         <Admin>
-            <div className="flex flex-col atur-text justify-center items-center mt-10">
-                <h1 className="text-6xl font-bold text-red-600">404</h1>
-                <h2 className="text-2xl mt-4 font-semibold">Akses Ditolak</h2>
-                <p className="text-lg mt-2">Maaf, Anda tidak memiliki akses ke halaman ini.</p>
+            <div className="flex flex-col items-center justify-center ">
+                <div className="text-center atur-text">
+                    {/* <p className='text-6xl'>Oops...</p> */}
+                    <img src={img404} id='img' alt="404 Not Found" className="object-cover w-full max-w-lg h-auto" />
+                    <p className=" text-lg text-gray-500">Akses di tolak</p>
+                    <p className=" text-lg text-gray-500">Maaf, Anda tidak memiliki akses ke halaman ini</p>
+                </div>
             </div>
         </Admin>
     )
