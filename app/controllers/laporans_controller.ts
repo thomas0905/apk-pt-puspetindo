4a9396cf5071568add0829aba371433a2f54f1df
Muhammad.Rois <111403383+roismohammed@users.noreply.github.com>
2024-08-16 08:03:00 +0700

    Update laporans_controller.ts

--- app/controllers/laporans_controller.ts
+++ app/controllers/laporans_controller.ts
@@ -5,7 +5,7 @@ export default class LaporansController {
     async laporan({ inertia, auth, response }: HttpContext) {
         const user = auth.user;
         const karyawan = await Karyawan.query().where('user_id', user?.id).first()
-        if(karyawan?.jabatan !== 'IT Softwa'){
+        if(karyawan?.jabatan !== 'IT Software'){
             response.abort('Kamu tidak memiliki kewenangan untuk melihat halaman ini', 403)
             
         }
