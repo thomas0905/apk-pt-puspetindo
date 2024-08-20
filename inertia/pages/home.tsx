b14eb4af4a0b590f1cb24e785476adb5121c3c5e
Muhammad.Rois <111403383+roismohammed@users.noreply.github.com>
2024-08-20 22:09:19 +0700

    iyf

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -34,8 +34,8 @@ export default function Index() {
           </div>
 
           <div className='mt-5'>
-            <h1 className='text-2xl font-bold '>Halo 👏 {data_karyawan.nama} .</h1>
-            {/* <p className='text-gray-600 text-sm'>Aktivitas apa yang akan kamu lakukan hari ini?</p> */}
+            <h1 className='text-2xl font-bold '>Halo 👏 {data_karyawan.nama} </h1>
+            <p className='text-gray-600 text-sm'>Aktivitas apa yang akan kamu lakukan hari ini?</p>
           </div>
 
           {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 mt-4">
