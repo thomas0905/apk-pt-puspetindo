693ef23e07b734deabac195605a8239bf0c7e34b
Muhammad.Rois <111403383+roismohammed@users.noreply.github.com>
2024-08-20 21:26:50 +0700

    df

--- app/controllers/home_controller.ts
+++ app/controllers/home_controller.ts
@@ -1,7 +1,8 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class HomeController {
-    async index({inertia}:HttpContext){
+    async index({inertia,auth}:HttpContext){
+        const user = auth.user;
         return inertia.render('home')
     }
 }
