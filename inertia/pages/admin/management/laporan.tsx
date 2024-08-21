b1b34e527d93e1773adef8c018ebd1407560fa05
Muhammad.Rois <111403383+roismohammed@users.noreply.github.com>
2024-08-21 21:17:00 +0700

    sdf

--- inertia/pages/admin/management/laporan.tsx
+++ inertia/pages/admin/management/laporan.tsx
@@ -7,11 +7,49 @@ import Admin from '~/layout/admin'
 import logoPuspetindo from '../../../img/logo-puspetindo.png'
 import ReactToPrint from 'react-to-print';
 import Swal from 'sweetalert2'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+const invoices = [
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+]
+
 
 export default function Laporan() {
-  const {data_manhours} = usePage().props
+  const { data_manhours } = usePage().props
   console.log(data_manhours);
-  
+
   const componentRef = useRef(null);
 
   const handlePrint = () => {
@@ -19,7 +57,7 @@ export default function Laporan() {
       title: 'Data Berhasil Di Tambah!',
       icon: 'success',
       confirmButtonText: 'Okee',
-  });
+    });
   }
   return (
     <Admin>
@@ -32,10 +70,36 @@ export default function Laporan() {
             <div className='flex justify-center'>
               <img src={logoPuspetindo} alt="Logo Puspetindo" />
             </div>
+            <h6 className='text-gray-600 mx-3 text-lg font-bold'>Laporan</h6>
             <div className='flex justify-between'>
               <div>
-                <h6 className='text-gray-600 text-lg font-bold'>Laporan</h6>
               </div>
+              <Table className='mt-2'>
+                <TableHeader className='bg-blue-300'>
+                  <TableRow className='border-t'>
+                    <TableHead className="w-[100px]">Invoice</TableHead>
+                    <TableHead>Status</TableHead>
+                    <TableHead>Method</TableHead>
+                    <TableHead className="text-right">Amount</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {invoices.map((invoice) => (
+                    <TableRow key={invoice.invoice}>
+                      <TableCell className="font-medium">{invoice.invoice}</TableCell>
+                      <TableCell>{invoice.paymentStatus}</TableCell>
+                      <TableCell>{invoice.paymentMethod}</TableCell>
+                      <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+                <TableFooter>
+                  <TableRow>
+                    <TableCell colSpan={3}>Total</TableCell>
+                    <TableCell className="text-right">$2,500.00</TableCell>
+                  </TableRow>
+                </TableFooter>
+              </Table>
             </div>
           </div>
           <div className='mt-2 flex justify-end'>
