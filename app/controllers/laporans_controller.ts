738930963a897a9c5358ddafeed9d5fd5757b939
Muhammad.Rois <111403383+roismohammed@users.noreply.github.com>
2024-08-26 20:59:47 +0700

    sdf

--- app/controllers/laporans_controller.ts
+++ app/controllers/laporans_controller.ts
@@ -10,25 +10,27 @@ export default class LaporansController {
             return response.redirect('/login');
         }
 
-        let man_hours;
-        if (request.input('start_date') != null) {
-            man_hours = await ManHour.query().whereBetween('tanggal', [request.input('start_date'), request.input('end_date')])
-            console.log(man_hours);
-            
-        }else{
-            man_hours = await ManHour.query()
-            .preload('karyawan')
-            .preload('proyek')
-            .where('karyawan_id', karyawan.id)
-        }
-
 
         const karyawan = await Karyawan.query().where('user_id', user.id).first();
         if (!karyawan || karyawan.jabatan !== 'IT Software') {
             return inertia.render('admin/error/404');
         }
 
-        
+        let man_hours;
+        if (request.input('start_date') != null) {
+            man_hours = await ManHour.query().whereBetween('tanggal', [request.input('start_date'), request.input('end_date')]).preload('karyawan').preload('proyek')
+            console.log(man_hours);
+
+        } else {
+            man_hours = await ManHour.query()
+                .preload('karyawan')
+                .preload('proyek')
+                .where('karyawan_id', karyawan.id)
+        }
+
+
+
+
         // const manhours = await ManHour.query().preload('karyawan').preload('proyek')
 
         return inertia.render('admin/management/laporan', {
