dd235831625852fa4d8bfe2171a3374d841d0196
Muhammad.Rois <111403383+roismohammed@users.noreply.github.com>
2024-09-09 09:21:42 +0700

    update dasboard controller

--- inertia/pages/admin/dasboard/departemen/create.tsx
+++ inertia/pages/admin/dasboard/departemen/create.tsx
@@ -1,13 +1,9 @@
-import { Link, useForm, usePage } from '@inertiajs/react';
-import { IconHome } from '@tabler/icons-react';
-import React, { FormEventHandler, useState } from 'react';
-import { ToastContainer } from 'react-toastify';
+import { useForm } from '@inertiajs/react';
+import React, { FormEventHandler, Fragment, useState } from 'react';
 import Swal from 'sweetalert2';
 import { Button } from '~/components/ui/button';
-import { Card } from '~/components/ui/card';
 import { Input } from '~/components/ui/input';
 import { Label } from '~/components/ui/label';
-import Admin from '~/layout/admin';
 
 export default function Create() {
     const { data, setData, post, processing } = useForm({
@@ -46,7 +42,7 @@ export default function Create() {
     };
 
     return (
-        <>
+        <Fragment>
             <form onSubmit={handleSubmit}>
                 <div className="grid gap-4 py-4">
                     <div className="grid-cols-4 items-center gap-4">
@@ -63,6 +59,6 @@ export default function Create() {
                 </div>
                 <Button className='bg-blue-600 hover:bg-blue-500' type="submit" disabled={processing}>Simpan</Button>
             </form>
-        </>
+        </Fragment>
     );
 }
