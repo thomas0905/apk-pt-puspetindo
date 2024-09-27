46530a098599533ea42ef31d13cc2eee664ee1e7
Muhammad.Rois <111403383+roismohammed@users.noreply.github.com>
2024-09-27 09:42:45 +0700

    update link aktif laporan tiketing

--- inertia/components/sidebar.tsx
+++ inertia/components/sidebar.tsx
@@ -95,7 +95,7 @@ export default function Sidebar({ isSidebarHidden, toggleSidebar }) {
 
                                 <Link
                                     href='/tiketing/laporan'
-                                className={`flex pl-2 text-black rounded-sm p-1 gap-2 text-md transition-all duration-200 ${isActive('/tiketing/laporanr') ? 'bg-blue-600 text-white' : 'hover:text-white hover:bg-blue-600'} ${isSidebarHidden ? 'w-18 justify-center p-2 relative group' : 'w-[190px]'}`}
+                                className={`flex pl-2 text-black rounded-sm p-1 gap-2 text-md transition-all duration-200 ${isActive('/tiketing/laporan') ? 'bg-blue-600 text-white' : 'hover:text-white hover:bg-blue-600'} ${isSidebarHidden ? 'w-18 justify-center p-2 relative group' : 'w-[190px]'}`}
                                 >
                                     <IconBook2 size={21} />
                                     {isSidebarHidden ? (
