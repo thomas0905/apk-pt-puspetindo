4f7fad30947b2184b81548c7b728601f93e2a727
Muhammad.Rois <111403383+roismohammed@users.noreply.github.com>
2024-10-01 20:07:04 +0700

    update tiketing laporan permission

--- app/controllers/tiketings_controller.ts
+++ app/controllers/tiketings_controller.ts
@@ -12,19 +12,18 @@ export default class TiketingsController {
     }
 
     async laporan({ inertia, response, auth }: HttpContext) {
-        const user = auth.user;
+        const user = await auth.user;
         if (!user) {
             return response.redirect('/login');
         }
         const karyawan = await Karyawan.query().where('user_id', user.id).first();
-        if (!karyawan || karyawan.jabatan !== 'IT Softwware') {
+        if (!karyawan || karyawan.jabatan !== 'IT Software') {
             return inertia.render('admin/error/404');
         }
         const tiketing = await Tiketing.query();
-        const data_karyawan = karyawan ? [karyawan] : [];
         return inertia.render('admin/dasboard/tiketing/laporan', {
             data_tiketing: tiketing,
-            data_karyawan: data_karyawan
+            data_karyawan: [karyawan]
         });
     }
     
